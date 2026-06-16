551b8a91fcd1976e9c01a157e5c91461f72fd10b
daivik89 <daivikpatel89@gmail.com>
2026-06-16 18:05:03 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+name ="daivik"
+age = 17
+price =20000.8976
+old= True
+a= None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
---
+++ firstprogram.py
@@ -0,0 +1,2 @@
+print("hello world")
+print("daivik is my name.","I am 17 years old.")
